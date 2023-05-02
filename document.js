4161b8f0c5c659d184dbe66b0696b36182b6d40a
Erik Lems <erik.lems@pagefive.nl>
2023-05-02 15:52:22 +0200

    Add files via upload

---
+++ document.js
@@ -0,0 +1,19 @@
+let arr = [];
+document.querySelectorAll(".record").forEach(rec => {
+     
+    rec.addEventListener("touchmove",ev=>{
+        rec.style.transition = '';   
+        document.querySelector(".bmain").children[Object.values(document.querySelectorAll(".record")).indexOf(rec)].style.transition = '';
+        document.querySelector(".bmain").children[Object.values(document.querySelectorAll(".record")).indexOf(rec)].style.background = `green`;
+        rec.addEventListener("touchend",(ev)=>{
+            arr = [];
+            rec.style.transform = 'initial';
+            document.querySelectorAll(".brecord").forEach(x => x.style.background = 'initial');
+            rec.style.transition = 'all 0.6s ease';
+            document.querySelector(".bmain").children[Object.values(document.querySelectorAll(".record")).indexOf(rec)].style.transition = 'all 1s ease';
+        })
+        arr.push(ev.touches[0].clientX);
+        const dist = arr.slice(-1) - arr[0];
+        rec.style.transform = `translateX(${dist}px)`; 
+    })
+})
