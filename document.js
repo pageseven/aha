25db535b38a92f82356fad769d2c36e887eb5e51
Erik Lems <erik.lems@pagefive.nl>
2023-05-03 14:24:54 +0200

    Add files via upload

--- document.js
+++ document.js
@@ -1,12 +1,16 @@
 let arr = [];document.body.style.height = `${window.innerHeight}px`;
+const absoluteValue = n => n>0 ? n : 0-n;
+const ist = d => (absoluteValue(d) / parseInt(window.getComputedStyle(document.querySelector(".record")).width));
 window.addEventListener("resize",()=> document.body.style.height = `${window.innerHeight}px`);
 document.querySelectorAll(".record").forEach(rec => {
      
     rec.addEventListener("touchmove",ev=>{
+        
         rec.style.transition = '';   
         document.querySelector(".bmain").children[Object.values(document.querySelectorAll(".record")).indexOf(rec)].style.transition = '';
-        document.querySelector(".bmain").children[Object.values(document.querySelectorAll(".record")).indexOf(rec)].style.background = `green`;
+     
         rec.addEventListener("touchend",(ev)=>{
+            rec.style.backgroundColor = 'initial';
             arr = [];
             rec.style.transform = 'initial';
             document.querySelectorAll(".brecord").forEach(x => x.style.background = 'initial');
@@ -15,6 +19,11 @@ document.querySelectorAll(".record").forEach(rec => {
         })
         arr.push(ev.touches[0].clientX);
         const dist = arr.slice(-1) - arr[0];
+        const color = dist > 0 ? '100' : '340';
+        const relativeDist =  ist(dist);
         rec.style.transform = `translateX(${dist}px)`; 
+        rec.style.backgroundColor = relativeDist > 0.5 ? `hsla(0,100%,100%,${1.7-relativeDist})` : 'white';
+        document.querySelector(".bmain").children[Object.values(document.querySelectorAll(".record")).indexOf(rec)].style.background = `hsla(${color},100%,50%,${relativeDist})`;
+       
     })
 })
